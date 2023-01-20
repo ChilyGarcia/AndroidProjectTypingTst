f0d07bf3c99becea6d77ebbfcf9d3968d2cbd90c
ChilyGarcia <jugada160324@gmail.com>
2023-01-19 20:36:52 -0500

    Nuevo

--- app/src/main/java/com/example/dedosrapidos_pruebadeescriturarpida/HistorialResultados.java
+++ app/src/main/java/com/example/dedosrapidos_pruebadeescriturarpida/HistorialResultados.java
@@ -35,24 +35,5 @@ public class HistorialResultados extends AppCompatActivity {
 
 
 
-
-
-        /*
-
-        listaResultados = findViewById(R.id.listaContactos);
-        listaResultados.setLayoutManager(new LinearLayoutManager(this));
-
-        Resultados db = new Resultados();
-
-        listaArrayResultadosPruebas = new ArrayList<>();
-
-        ListaContactosAdapter adapter = new ListaContactosAdapter(db.listaResultadosPruebas());
-        //Enviar la informacion estruturada para que lo pueda procesar correctamente el listView
-        listaResultados.setAdapter(adapter);
-
-
-         */
-
-
     }
 }
